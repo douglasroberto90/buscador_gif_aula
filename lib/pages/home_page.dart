ebd9e068a4353b0e9d7695cd0aeb711dbb2fad8d
douglasroberto90 <douglas_roberto90@hotmail.com>
2024-02-22 22:26:20 -0300

    Initial commit
    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:buscador_gif_aula/repositories/repositorio.dart';
+import 'package:flutter/material.dart';
+
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Image.asset("assets/images/logo.png"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Digite sua pesquisa"),
+          ),
+          Container(),
+        ],
+      ),
+    );
+  }
+}
